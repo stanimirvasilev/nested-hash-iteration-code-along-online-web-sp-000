805fee0680bbda2fd6332875f7b5442f1903e953
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-03-06 13:50:33 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,7 +17,7 @@ def remove_strawberry(contacts)
 
 contacts.each do |person, contact_details_hash|
   if person == "Freddy Mercury" 
-    contact_details_hash.each do |atrtibute, data|
+    contact_details_hash.each do |attribute, data|
       
     end  
   end 
